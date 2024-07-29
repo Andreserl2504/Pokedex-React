ee8a8a7ccc683ecdcdcab4702d4579394439a17d
Andrés Rodríguez <andres.rodriguezv05@gmail.com>
2024-07-29 13:50:04 -0400

    imgtypes public

--- src/components/ImgTypes.jsx
+++ src/components/ImgTypes.jsx
@@ -3,20 +3,20 @@ export const ImgTypes = (types) => {
     <>
       <img
         title={types.Types[0]}
-        src={`/src/assets/SVG/pokemon_types/Pokemon_Type_Icon_${types.Types[0]}.svg`}
+        src={`/public/pokemon_types/Pokemon_Type_Icon_${types.Types[0]}.svg`}
         alt={types.Types[0]}
       />
 
       <img
         title={types.Types[1]}
-        src={`/src/assets/SVG/pokemon_types/Pokemon_Type_Icon_${types.Types[1]}.svg`}
+        src={`/public/pokemon_types/Pokemon_Type_Icon_${types.Types[1]}.svg`}
         alt={types.Types[1]}
       />
     </>
   ) : (
     <img
       title={types.Types[0]}
-      src={`/src/assets/SVG/pokemon_types/Pokemon_Type_Icon_${types.Types[0]}.svg`}
+      src={`/public/pokemon_types/Pokemon_Type_Icon_${types.Types[0]}.svg`}
       alt={types.Types[0]}
     />
   );
